356784ecbd1972327677e934242b13008ca61b23
Muhammad Shareyar <shareyarz40@gmail.com>
2025-07-17 18:25:11 +0200

    app layout setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
       },
     },
   },
