cc3ba808d30bf15c8010b6fc5014bfc3d6eb75ce
Muhammad Shareyar <shareyarz40@gmail.com>
2025-07-17 21:03:24 +0200

    add metadata and assets files

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Property Pulse",
+  description: "Find the best property near you!",
+  keywords: "real estate, property, rental, housing, rest rooms",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
