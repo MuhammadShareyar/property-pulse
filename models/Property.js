eee253554e74feefc860c25532a4c19ba7e4f04c
Muhammad Shareyar <shareyarz40@gmail.com>
2025-07-18 17:56:54 +0200

    setup mongodb and create user and property models

---
+++ models/Property.js
@@ -0,0 +1,66 @@
+import { Schema, model, models } from "mongoose";
+
+const PropertySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["Apartment", "House", "Studio", "Villa", "Condo"], // expand as needed
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+    },
+    amenities: {
+      type: [String],
+    },
+    rates: {
+      weekly: Number,
+      monthly: Number,
+    },
+    seller_info: {
+      name: String,
+      email: String,
+      phone: String,
+    },
+    images: {
+      type: [String],
+    },
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Property = models.Property || model("Property", PropertySchema);
+
+export default Property;
