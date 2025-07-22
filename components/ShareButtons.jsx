a961732b456ff45a64cbe14693698dae7442761c
Muhammad Shareyar <shareyarz40@gmail.com>
2025-07-22 12:29:11 +0200

    add social share buttons

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,10 +1,56 @@
-import { FaShare } from "react-icons/fa";
+"use client";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  EmailIcon,
+} from "react-share";
 
 const ShareButtons = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="mr-2" /> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        Share this property
+      </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${property.type.replace(/\s/g, "")}ForRent`}
+          className="hover:scale-110 transition-transform"
+        >
+          <FacebookIcon size={40} round />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`${property.type.replace(/\s/g, "")}ForRent`]}
+          className="hover:scale-110 transition-transform"
+        >
+          <TwitterIcon size={40} round />
+        </TwitterShareButton>
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property.name}
+          className="hover:scale-110 transition-transform"
+        >
+          <WhatsappIcon size={40} round />
+        </WhatsappShareButton>
+        <EmailShareButton
+          url={shareUrl}
+          subject={property.name}
+          body={`Check out this property: ${shareUrl}`}
+          className="hover:scale-110 transition-transform"
+        >
+          <EmailIcon size={40} round />
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 
