f2d8ad887d46fe0112221383ae7982ba9c82c87d
Muhammad Shareyar <shareyarz40@gmail.com>
2025-07-23 09:51:42 +0200

    implement swipebox for properties gallery

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,35 +1,69 @@
-import Image from "next/image";
+"use client";
 
-const PropertyImages = ({images}) => {
+import Image from "next/image";
+import { Gallery, Item } from "react-photoswipe-gallery";
+
+const PropertyImages = ({ images }) => {
   return (
-    <section className="bg-blue-50 p-4">
-      <div className="container mx-auto">
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt=""
-            width={1800}
-            height={400}
-            priority={true}
-            className="object-cover h-[400px] w-full rounded-xl cursor-pointer"
-          />
-        ) : (
-          <div className="grid grid-cols-2 gap-4">
-            {images.map((image, index) => (
-              <div className={`${images.length === 3 && index===2 ? "col-span-2" : "col-span-1"}`} key={index}>
+    <Gallery>
+      <section className="bg-blue-50 p-4">
+        <div className="container mx-auto">
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width={1000}
+              height={600}
+            >
+              {({ ref, open }) => (
                 <Image
-                  src={image}
+                  ref={ref}
+                  onClick={open}
+                  src={images[0]}
                   alt=""
                   width={1800}
                   height={400}
+                  priority={true}
                   className="object-cover h-[400px] w-full rounded-xl cursor-pointer"
                 />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              )}
+            </Item>
+          ) : (
+            <div className="grid grid-cols-2 gap-4">
+              {images.map((image, index) => (
+                <div
+                  className={`${
+                    images.length === 3 && index === 2
+                      ? "col-span-2"
+                      : "col-span-1"
+                  }`}
+                  key={index}
+                >
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width={1000}
+                    height={600}
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        ref={ref}
+                        onClick={open}
+                        src={image}
+                        alt=""
+                        width={1800}
+                        height={400}
+                        className="object-cover h-[400px] w-full rounded-xl cursor-pointer"
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   );
 };
 
